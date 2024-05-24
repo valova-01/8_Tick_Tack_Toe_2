58f8892697fb7b968444540a136a7d0477056833
anna <anna.valova.01@mail.ru>
2024-05-24 14:00:59 +0300

    add app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import  Game  from './components/Game/Game';
+import Game from './components/Game/Game';
 import styles from './App.module.css';
 
 export const App = () => {
-	const currentPlayer = 'X';
-	const isGameEnded = false;
-	const isDraw = false;
-
 	return (
 		<div className={styles.App}>
-			<Game currentPlayer={currentPlayer} isGameEnded={isGameEnded} isDraw={isDraw} />
+			<Game />
 		</div>
 	);
 };
